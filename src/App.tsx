f28692a4e0547a1daad38f4510b66708d635c251
Mustafa önler <monler049@gmail.com>
2026-02-17 14:29:49 +0300

    ilk kayıt

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> web tasarımı ve programlama</h1>
+        <h2>LAB-1</h2>
+        <p>Ad soyad: Mustafa Önler</p>
+        <p>öğrenci numarası:245541017</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
